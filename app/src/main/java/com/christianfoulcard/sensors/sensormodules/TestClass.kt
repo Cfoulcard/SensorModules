ab2edbf76535e36131d5f29f684dcf59c919c124
Taterchip <cfoulcard@gmail.com>
2021-06-27 17:38:32 -0500

    Text Test

---
+++ app/src/main/java/com/christianfoulcard/sensors/sensormodules/TestClass.kt
@@ -0,0 +1,21 @@
+package com.christianfoulcard.sensors.sensormodules
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.christianfoulcard.sensors.lightsensor.SensorLight
+import com.christianfoulcard.sensors.lightsensor.Toaster
+
+class TestClass : AppCompatActivity(), SensorLight {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.textView)
+
+        val toaster = Toaster()
+        textView.text = toaster.toasterMessage
+    }
+
+}
---
+++ lightsensor/src/main/java/com/christianfoulcard/sensors/lightsensor/Toaster.kt
@@ -0,0 +1,9 @@
+package com.christianfoulcard.sensors.lightsensor
+
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class Toaster {
+    val toasterMessage = "Test Toast Message"
+
+}
